c6fcbe3bf1ef041516cd992379fdf3ef6f65dc2a
Saurav Kumar <ksksaurav@gmail.com>
2023-10-06 00:39:38 +0530

    Modified Stream3 to analyse parallel and sequential stream performance

--- src/com/winterbe/java8/samples/stream/Streams3.java
+++ src/com/winterbe/java8/samples/stream/Streams3.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 /**
  * @author Benjamin Winterberg
@@ -16,19 +17,23 @@ public class Streams3 {
         List<String> values = new ArrayList<>(MAX);
         for (int i = 0; i < MAX; i++) {
             UUID uuid = UUID.randomUUID();
+//            System.out.println("The value is "+uuid.toString());
             values.add(uuid.toString());
         }
 
         // sequential
 
-        long t0 = System.nanoTime();
+        long t0 =System.nanoTime();
+//        long t0 = System.nanoTime();
 
         long count = values.stream().sorted().count();
+//        long count = values.stream().sorted().count();
         System.out.println(count);
 
         long t1 = System.nanoTime();
 
         long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+//        long millis = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
         System.out.println(String.format("sequential sort took: %d ms", millis));
     }
 
@@ -44,6 +49,7 @@ public class Streams3 {
         long t0 = System.nanoTime();
 
         long count = values.parallelStream().sorted().count();
+//        long count = values.parallelStream().sorted().count();
         System.out.println(count);
 
         long t1 = System.nanoTime();
