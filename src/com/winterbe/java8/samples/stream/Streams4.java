91c325dbad66495d2b574263670796ce3f03a035
Saurav Kumar <ksksaurav@gmail.com>
2023-10-06 01:11:33 +0530

    Modified Stream4 to assess the IntStream methods

--- src/com/winterbe/java8/samples/stream/Streams4.java
+++ src/com/winterbe/java8/samples/stream/Streams4.java
@@ -9,29 +9,38 @@ import java.util.stream.IntStream;
 public class Streams4 {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 1) {
-                System.out.println(i);
-            }
-        }
+//        for (int i = 0; i < 10; i++) {
+//            if (i % 2 == 1) {
+//                System.out.println(i);
+//            }
+//        }
 
-        IntStream.range(0, 10)
-            .forEach(i -> {
-                if (i % 2 == 1) System.out.println(i);
-            });
+        IntStream.range(0,10)
+                        .forEach( i -> {
+                            if (i%2==1) System.out.println(i);
+                        });
+//        IntStream.range(0, 10)
+//            .forEach(i -> {
+//                if (i % 2 == 1) System.out.println(i);
+//            });
 
-        IntStream.range(0, 10)
-            .filter(i -> i % 2 == 1)
-            .forEach(System.out::println);
+        IntStream.range(0,10).filter(i -> i%2==1)
+                        .forEach(System.out::println);
+//        IntStream.range(0, 10)
+//            .filter(i -> i % 2 == 1)
+//            .forEach(System.out::println);
 
-        OptionalInt reduced1 =
-            IntStream.range(0, 10)
-                .reduce((a, b) -> a + b);
-        System.out.println(reduced1.getAsInt());
+        OptionalInt reduce = IntStream.range(0, 10).reduce((a, b) -> a + b);
+//        OptionalInt reduced1 =
+//            IntStream.range(0, 10)
+//                .reduce((a, b) -> a + b);
 
-        int reduced2 =
-            IntStream.range(0, 10)
-                .reduce(7, (a, b) -> a + b);
-        System.out.println(reduced2);
+        System.out.println("The value of summation " + reduce.getAsInt());
+
+        int reduce1 = IntStream.range(0, 10).reduce(10, (a, b) -> a + b);
+//        int reduced2 =
+//            IntStream.range(0, 10)
+//                .reduce(7, (a, b) -> a + b);
+        System.out.println(reduce1);
     }
 }
