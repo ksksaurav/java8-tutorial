171148bd5371c8cf810e2c9c028118b7594d2b90
Saurav Kumar <ksksaurav@gmail.com>
2023-10-07 02:03:31 +0530

    Modified src/com/winterbe/java8/samples/stream/Streams5.java file completly

--- src/com/winterbe/java8/samples/stream/Streams5.java
+++ src/com/winterbe/java8/samples/stream/Streams5.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
+import static java.lang.System.*;
 
 /**
  * Testing the order of execution.
@@ -20,49 +21,97 @@ public class Streams5 {
 
        // test1(strings);
 //        test2(strings);
-        test3(strings);
+//        test3(strings);
 //        test4(strings);
 //        test5(strings);
 //        test6(strings);
 //        test7(strings);
- //       test8(strings);
+        test8(strings);
     }
 
     private static void test8(List<String> stringCollection) {
-        Supplier<Stream<String>> streamSupplier =
-            () -> stringCollection
-                .stream()
-                .filter(s -> s.startsWith("a"));
 
-        streamSupplier.get().anyMatch(s -> true);
-        streamSupplier.get().noneMatch(s -> true);
+
+        Supplier<Stream<String>> streamSupplier = () -> stringCollection
+                .stream()
+                .filter((s -> s.startsWith("a")));
+
+
+//        Supplier<Stream<String>> streamSupplier =
+//            () -> stringCollection
+//                .stream()
+//                .filter(s -> s.startsWith("a"));
+
+        boolean d = streamSupplier.get().anyMatch(s -> {
+            out.println(s);
+            return s.startsWith("d");
+        });
+
+        boolean l = streamSupplier.get().noneMatch(s -> {
+            out.println(s);
+            return s.startsWith("k");
+        });
+
+         out.println(" The value of b and l " + d + " :"+l );
+//        streamSupplier.get().anyMatch(s -> true);
+//        streamSupplier.get().noneMatch(s -> true);
     }
 
     // stream has already been operated upon or closed
     private static void test7(List<String> stringCollection) {
-        Stream<String> stream = stringCollection
-            .stream()
-            .filter(s -> s.startsWith("a"));
 
-        stream.anyMatch(s -> true);
-        stream.noneMatch(s -> true);
+        stringCollection.stream().forEach(out::println);
+
+        Stream<String> stream = stringCollection
+                .stream()
+                .filter(s -> s.startsWith("a"));
+//        Stream<String> stream = stringCollection
+//            .stream()
+//            .filter(s -> s.startsWith("a"));
+//         Stream<String> out = stream;
+          out.println(" The value of anyMatch is  " +stream.anyMatch(s->{
+              out.println(s);return true;}));
+        Stream<String> stream1 = stringCollection
+                .stream()
+                .filter(s -> s.startsWith("a"));
+          out.println("The value of nonematch " + stream1.noneMatch(s-> {
+              out.println(s);
+              return s.startsWith("b");
+          }));
+//        stream.anyMatch(s -> true);
+//        stream.noneMatch(s -> true);
     }
 
     // short-circuit
     private static void test6(List<String> stringCollection) {
+
         stringCollection
-            .stream()
-            .map(s -> {
-                System.out.println("map:      " + s);
-                return s.toUpperCase();
-            })
-            .anyMatch(s -> {
-                System.out.println("anyMatch: " + s);
-                return s.startsWith("A");
-            });
+                .stream()
+                        .map(s->{
+                            out.println("map : %s "+s);
+                            return s.toUpperCase();
+                        })
+                                .anyMatch(s-> {
+                                    out.println("anyMatch : "+s);
+                                    return s.startsWith("A");
+                                });
+
+//        stringCollection
+//            .stream()
+//            .map(s -> {
+//                System.out.println("map:      " + s);
+//                return s.toUpperCase();
+//            })
+//            .anyMatch(s -> {
+//                System.out.println("anyMatch: " + s);
+//                return s.startsWith("A");
+//            });
     }
 
     private static void test5(List<String> stringCollection) {
+
+
+
         stringCollection
             .stream()
             .filter(s -> {
@@ -82,6 +131,7 @@ public class Streams5 {
 
     // sorted = horizontal
     private static void test4(List<String> stringCollection) {
+
         stringCollection
             .stream()
             .sorted((s1, s2) -> {
@@ -100,6 +150,8 @@ public class Streams5 {
     }
 
     private static void test3(List<String> stringCollection) {
+
+
         stringCollection
             .stream()
             .filter(s -> {
